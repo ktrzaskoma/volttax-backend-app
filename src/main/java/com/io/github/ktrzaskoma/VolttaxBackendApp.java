94a51ca3dab3a0719ec7406c77d109e96de21422
ktrzaskoma <kttrzaskoma@gmail.com>
2023-06-24 18:55:48 +0200

    init

---
+++ src/main/java/com/io/github/ktrzaskoma/VolttaxBackendApp.java
@@ -0,0 +1,13 @@
+package com.io.github.ktrzaskoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolttaxBackendApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolttaxBackendApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/github/ktrzaskoma/VolttaxBackendAppTests.java
@@ -0,0 +1,13 @@
+package com.io.github.ktrzaskoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolttaxBackendAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
